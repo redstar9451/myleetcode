2532c228a5ed4e5b9e14ae089ae838f1b0fde036
redstar <redstar9451@163.com>
2024-01-30 12:53:35 +0800

    add comment in p206

--- p0206/p206.go
+++ p0206/p206.go
@@ -9,9 +9,12 @@ func reverseList(head *ListNode) *ListNode {
 	if head == nil {
 		return nil
 	}
+	// 现把头节点放在尾部，此时头节点的next指向nil
 	tail := head.Next
 	head.Next = nil
+
 	newHead := head
+	// 每次把当前节点放在头部，然后把当前节点的next指向新的头部
 	for tail != nil {
 		tmp := tail.Next
 		tail.Next = newHead
